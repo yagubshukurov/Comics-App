167db3ed5c7c9c0cb59e6cb367dc22d017f6bbc7
Yagub Shukurov <shukurov23@outlook.com>
2020-02-15 11:36:06 +0100

    Initial Commit

---
+++ HW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW
+//
+//  Created by Yagub Shukurov on 2/15/20.
+//  Copyright © 2020 Yagub Shukurov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
